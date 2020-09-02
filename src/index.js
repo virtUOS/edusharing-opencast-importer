eba5cd735327147828052ac23f7680c4bcc44f03
Florian Feyen <florian@feyen.net>
2020-09-02 15:04:30 +0200

    draft base structure for app (development)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 'use strict'
-
 const logger = require('node-file-logger')
 const CONF = require('./config/config.json')
 
-const opencast = require('./opencast/opencast-http.js')
+const storage = require('./services/data-storage.js')
+const getAllPublishedEpisodes = require('./opencast/get-all-published-episodes.js')
+const getSeriesIdsFromEpisodes = require('./opencast/get-series-from-episodes.js')
 
 logger.SetUserOptions(CONF.logger)
 
-function main() {
+async function main() {
   logger.Info('Application started')
-  opencast.getAllPublishedEpisodes()
-    .then((episodes) => {
-      console.log(episodes.length)
+
+  let ocEpisodes = []
+  let ocSeries = []
+
+  getAllPublishedEpisodes.start()
+    .then(async(episodes) => {
+      ocEpisodes = episodes
+      ocSeries = await getSeriesIdsFromEpisodes.start(episodes)
+    })
+    .then(() => {
+      storage.storeData(CONF.oc.filenames.episodes, ocEpisodes)
+      console.log(ocEpisodes.length)
+      console.log(ocSeries.length)
     })
     .catch(error => logger.Error(error))
 }
