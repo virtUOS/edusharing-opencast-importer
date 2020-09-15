d0c1a12600d4b683a1b6bf0465ed22504ec044af
Florian Feyen <florian@feyen.net>
2020-09-15 11:18:50 +0200

    require dotenv in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 'use strict'
+require('dotenv').config()
 const logger = require('node-file-logger')
 const CONF = require('./config/config.json')
 
@@ -6,9 +7,8 @@ const storage = require('./services/data-storage.js')
 const getAllPublishedEpisodes = require('./opencast/get-all-published-episodes.js')
 const getSeriesIdsFromEpisodes = require('./opencast/get-series-from-episodes.js')
 
-logger.SetUserOptions(CONF.logger)
-
 async function main() {
+  logger.SetUserOptions(CONF.logger)
   logger.Info('Application started')
 
   let ocEpisodes = []
@@ -22,9 +22,9 @@ async function main() {
     })
     .then((series) => {
       ocSeries = series
-      storage.storeData(CONF.oc.filenames.series, series)
       console.log(ocEpisodes.length)
       console.log(ocSeries.length)
+      storage.storeData(CONF.oc.filenames.series, series)
     })
     .catch(error => logger.Error(error))
 }
