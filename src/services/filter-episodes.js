af7b2674c2c71df149efe602d3efa7a43880be94
Florian Feyen <florian@feyen.net>
2020-10-01 18:00:36 +0200

    add filter draft (empty function)

---
+++ src/services/filter-episodes.js
@@ -0,0 +1,12 @@
+'use strict'
+
+function filterForOpenLicensedEpisodes(episodes) {
+  // const filteredEpisodes = episodes.filter(episode => episode.dcLicense === 'CC-BY')
+  const filteredEpisodes = episodes
+
+  return filteredEpisodes
+}
+
+module.exports = {
+  filterForOpenLicensedEpisodes
+}
