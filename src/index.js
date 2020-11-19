f484c0d16ce094351c2b7a0d3dd44fba442a0ace
Florian Feyen <florian@feyen.net>
2020-11-19 17:13:29 +0100

    add function to sort episodes and apply data to them; also to apply data series objects

--- src/index.js
+++ src/index.js
@@ -11,16 +11,19 @@ const filter = require('./services/filter-episodes')
 
 async function main() {
   logger.SetUserOptions(CONF.logger)
-  logger.Info('Application started')
+  logger.Info('[Application] Started')
 
   let ocEpisodes
   let ocSeries
-  let sortedEpisodesPerSeries
+  let seriesData
+  let uniqueEpisodes
+  let episodesData
+  const ocInstance = CONF.oc.develop.useDevDomain ? CONF.oc.domainDev : CONF.oc.domain
 
   async function initStoredData() {
-    ocEpisodes = await storage.loadData(CONF.oc.filenames.episodes)
-    ocSeries = await storage.loadData(CONF.oc.filenames.series)
-    sortedEpisodesPerSeries = await storage.loadData(CONF.oc.filenames.sortedEpisodes)
+    ocEpisodes = await storage.loadData(CONF.oc.filenames.episodes, ocInstance)
+    ocSeries = await storage.loadData(CONF.oc.filenames.series, ocInstance)
+    seriesData = await storage.loadData(CONF.oc.filenames.sortedEpisodeIdsPerSeries, ocInstance)
   }
 
   initStoredData().then(() => {
@@ -28,15 +31,22 @@ async function main() {
       .start(ocEpisodes, false)
       .then(async(episodes) => {
         ocEpisodes = filter.filterAllowedLicensedEpisodes(episodes, CONF.filter.allowedLicences)
-        // storage.storeData(CONF.oc.filenames.episodes, episodes)
+        // storage.storeData(CONF.oc.filenames.episodes, episodes, ocInstance)
         return await getSeriesIdsFromEpisodes.start(ocEpisodes, ocSeries, false)
       })
       .then((series) => {
         ocSeries = series
-        // storage.storeData(CONF.oc.filenames.series, series)
-        sortedEpisodesPerSeries = sorter.getSortedEpisodesPerSeriesIds(ocSeries, ocEpisodes)
-        storage.storeData(CONF.oc.filenames.sortedEpisodes, sortedEpisodesPerSeries)
-        console.log(sortedEpisodesPerSeries.length)
+        // storage.storeData(CONF.oc.filenames.series, series, ocInstance)
+        seriesData = sorter.getSortedEpisodesPerSeriesIds(ocSeries, ocEpisodes, ocInstance)
+        return seriesData
+      })
+      .then((seriesData) => {
+        uniqueEpisodes = sorter.getUniqueEpisodesObjects(ocEpisodes, seriesData)
+        return uniqueEpisodes
+      })
+      .then((uniqueEpisodes) => {
+        episodesData = sorter.applyEpisodeData(uniqueEpisodes, ocEpisodes, ocInstance)
+        storage.storeData(CONF.oc.filenames.episodesData, episodesData, ocInstance)
       })
       .catch((error) => logger.Error(error))
   })
--- src/services/sorter.js
+++ src/services/sorter.js
@@ -1,10 +1,11 @@
 'use strict'
 
-function getSortedEpisodesPerSeriesIds(series, filteredEpisodes) {
+function getSortedEpisodesPerSeriesIds(series, filteredEpisodes, ocInstance) {
   const uniqueSeriesIds = getUniqueSeriesIds(series)
   const seriesIdsObjects = createObjectsFromSeriesIds(uniqueSeriesIds)
   const sortedEpisodesPerSeries = sortEpisodesPerSeriesId(seriesIdsObjects, filteredEpisodes)
-  return sortedEpisodesPerSeries
+  const sortedSeriesData = applySeriesData(sortedEpisodesPerSeries, series, ocInstance)
+  return sortedSeriesData
 }
 
 function getUniqueSeriesIds(series) {
@@ -39,6 +40,78 @@ function sortEpisodesPerSeriesId(seriesIdsObjects, episodes) {
   return sortedEpisodes
 }
 
+function applySeriesData(sortedEpisodesPerSeries, ocSeries, ocInstance) {
+  return sortedEpisodesPerSeries.map((series) => {
+    const currentOcSeries = ocSeries[ocSeries.findIndex((ocSeries) => ocSeries.id === series.id)]
+
+    if (currentOcSeries) {
+      if (currentOcSeries.dcTitle) series.title = currentOcSeries.dcTitle
+      if (currentOcSeries.dcDescription) series.description = currentOcSeries.dcDescription
+      if (currentOcSeries.dcPublisher) series.publisher = currentOcSeries.dcPublisher
+      if (currentOcSeries.dcCreated) series.created = currentOcSeries.dcCreated
+      if (currentOcSeries.keywords.length > 0) series.keywords = currentOcSeries.keywords
+      if (currentOcSeries.mediaType) series.mediaType = currentOcSeries.mediaType
+      if (currentOcSeries.modified) series.modified = currentOcSeries.modified
+      series.from = ocInstance
+
+      return series
+    }
+  })
+}
+
+function getUniqueEpisodesObjects(filteredEpisodes) {
+  const uniqueEpisodeIds = getUniqueEpisodeIds(filteredEpisodes)
+  const sortedEpisodes = createObjectsFromEpisodeIds(uniqueEpisodeIds)
+  return sortedEpisodes
+}
+
+function getUniqueEpisodeIds(filteredEpisodes) {
+  const episodeIds = []
+  for (let i = 0; i < filteredEpisodes.length; i++) {
+    episodeIds.push(filteredEpisodes[i].id)
+  }
+  return [...new Set(episodeIds)]
+}
+
+function createObjectsFromEpisodeIds(uniqueEpisodeIds) {
+  return uniqueEpisodeIds.map((episodeId) => {
+    return {
+      id: episodeId
+    }
+  })
+}
+
+function applyEpisodeData(sortedEpisodes, ocEpisodes, ocInstance) {
+  return sortedEpisodes.map((episode) => {
+    const currentOcEpisode =
+      ocEpisodes[ocEpisodes.findIndex((ocEpisode) => ocEpisode.id === episode.id)]
+
+    if (currentOcEpisode) {
+      if (currentOcEpisode.dcExtent) episode.extent = currentOcEpisode.dcExtent
+      if (currentOcEpisode.dcTitle) episode.title = currentOcEpisode.dcTitle
+      if (currentOcEpisode.dcDescription) episode.description = currentOcEpisode.dcDescription
+      if (currentOcEpisode.dcCreator) episode.creator = currentOcEpisode.dcCreator
+      if (currentOcEpisode.dcCreated) episode.created = currentOcEpisode.dcCreated
+      if (currentOcEpisode.dcLicense) episode.license = currentOcEpisode.dcLicense
+      if (currentOcEpisode.dcIsPartOf) episode.isPartOf = currentOcEpisode.dcIsPartOf
+      if (currentOcEpisode.mediaType) episode.mediaType = currentOcEpisode.mediaType
+      if (currentOcEpisode.keywords.length > 0) episode.keywords = currentOcEpisode.keywords
+      if (currentOcEpisode.mediapackage.media.track.url) {
+        episode.url = currentOcEpisode.mediapackage.media.track.url
+      } else if (currentOcEpisode.mediapackage.media.track[0].url) {
+        episode.url = currentOcEpisode.mediapackage.media.track[0].url
+      }
+      if (currentOcEpisode.modified) episode.modified = currentOcEpisode.modified
+      episode.from = ocInstance
+
+      return episode
+    }
+  })
+}
+
 module.exports = {
-  getSortedEpisodesPerSeriesIds
+  getSortedEpisodesPerSeriesIds,
+  getUniqueEpisodesObjects,
+  applySeriesData,
+  applyEpisodeData
 }
