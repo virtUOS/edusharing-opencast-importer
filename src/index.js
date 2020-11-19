c6b10372b31ef73b2e7e224449880035e439dd65
Florian Feyen <florian@feyen.net>
2020-11-19 11:50:22 +0100

    add async function to init/load stored data

--- src/index.js
+++ src/index.js
@@ -7,37 +7,39 @@ const storage = require('./services/data-storage.js')
 const getAllPublishedEpisodes = require('./opencast/get-all-published-episodes.js')
 const getSeriesIdsFromEpisodes = require('./opencast/get-series-from-episodes.js')
 const sorter = require('./services/sorter.js')
+const filter = require('./services/filter-episodes')
 
 async function main() {
   logger.SetUserOptions(CONF.logger)
   logger.Info('Application started')
 
-  let ocEpisodes = await storage.loadData(CONF.oc.filenames.episodes)
-  let ocSeries = await storage.loadData(CONF.oc.filenames.series)
-  let sortedEpisodesPerSeries = await storage.loadData(
-    CONF.oc.filenames.sortedEpisodes
-  )
+  let ocEpisodes
+  let ocSeries
+  let sortedEpisodesPerSeries
 
-  getAllPublishedEpisodes
-    .start(ocEpisodes, true)
-    .then(async(episodes) => {
-      ocEpisodes = episodes
-      // storage.storeData(CONF.oc.filenames.episodes, episodes)
-      return await getSeriesIdsFromEpisodes.start(episodes, ocSeries, false)
-    })
-    .then((series) => {
-      ocSeries = series
-      console.log(ocEpisodes.length)
-      console.log(ocSeries.length)
-      // storage.storeData(CONF.oc.filenames.series, series)
-      sortedEpisodesPerSeries = sorter.getSortedEpisodesPerSeriesIds(
-        series,
-        ocEpisodes
-      )
-      // storage.storeData(CONF.oc.filenames.sortedEpisodes, sortedEpisodesPerSeries)
-      console.log(sortedEpisodesPerSeries.length)
-    })
-    .catch((error) => logger.Error(error))
+  async function initStoredData() {
+    ocEpisodes = await storage.loadData(CONF.oc.filenames.episodes)
+    ocSeries = await storage.loadData(CONF.oc.filenames.series)
+    sortedEpisodesPerSeries = await storage.loadData(CONF.oc.filenames.sortedEpisodes)
+  }
+
+  initStoredData().then(() => {
+    getAllPublishedEpisodes
+      .start(ocEpisodes, false)
+      .then(async(episodes) => {
+        ocEpisodes = filter.filterAllowedLicensedEpisodes(episodes, CONF.filter.allowedLicences)
+        // storage.storeData(CONF.oc.filenames.episodes, episodes)
+        return await getSeriesIdsFromEpisodes.start(ocEpisodes, ocSeries, false)
+      })
+      .then((series) => {
+        ocSeries = series
+        // storage.storeData(CONF.oc.filenames.series, series)
+        sortedEpisodesPerSeries = sorter.getSortedEpisodesPerSeriesIds(ocSeries, ocEpisodes)
+        storage.storeData(CONF.oc.filenames.sortedEpisodes, sortedEpisodesPerSeries)
+        console.log(sortedEpisodesPerSeries.length)
+      })
+      .catch((error) => logger.Error(error))
+  })
 }
 
 main()
