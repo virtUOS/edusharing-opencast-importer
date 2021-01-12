4f4703610ece256dfc83183bc4f5c96ce4995b7b
Florian Feyen <florian@feyen.net>
2021-01-12 12:15:17 +0100

    sort getBodyUpdateMetadata() JSON data and remove conditional in Promise producer loop

--- src/edu-sharing/update-metadata.js
+++ src/edu-sharing/update-metadata.js
@@ -20,7 +20,6 @@ async function updateMetadata(ocInstance, episodesData, authObj) {
     const requests = []
     for (let i = 0; i < episodesData.length; i++) {
       if (!episodesData[i].nodeId) continue
-      if (!modifiedSinceLastUpdate(episodesData[i])) continue
 
       requests.push(
         limit(() =>
@@ -30,19 +29,13 @@ async function updateMetadata(ocInstance, episodesData, authObj) {
             getHeadersUpdateMetadata(authObj),
             i
           ).catch((error) => {
-            logger.Error(error)
+            return error
           })
         )
       )
     }
 
     return Promise.all(requests)
-  }
-
-  function modifiedSinceLastUpdate(episode) {
-    if (episode.lastUpdated < episode.modified) return true
-    if (episode.lastUpdated < episode.created) return true
-    return false
   }
 
   async function sendPostRequest(url, body, headers, index) {
@@ -54,6 +47,7 @@ async function updateMetadata(ocInstance, episodesData, authObj) {
         }
       })
       .catch((error) => {
+        console.log(error.response.data)
         logger.Error('[ES API] ' + error)
       })
   }
@@ -79,84 +73,84 @@ async function updateMetadata(ocInstance, episodesData, authObj) {
       .toUpperCase()
 
     return JSON.stringify({
-      // 'ccm:lifecyclecontributer_authorVCARD_ORG': [''],
-      // 'ccm:lifecyclecontributer_authorVCARD_TITLE': [''],
       'ccm:original': [episode.nodeId],
-      // 'cm:created': ['1605880817805'],
-      // 'virtual:commentcount': ['0'],
-      // 'ccm:metadatacontributer_creatorVCARD_ORG': [''],
-      // 'cclom:version': ['1.3'],
       'virtual:licenseicon': [
         `http://localhost/edu-sharing/ccimages/licenses/${licenseLowerDash}.svg`
       ],
-      // 'virtual:usagecount': ['0'],
       'sys:node-uuid': [episode.nodeId],
-      // >> 'ccm:lifecyclecontributer_authorVCARD_SURNAME': ['Nachname'],
-      // 'ccm:lifecyclecontributer_authorVCARD_URL': [''],
-      // 'virtual:childobjectcount': ['0'],
       'virtual:licenseurl': [
         `https://creativecommons.org/licenses/${licenseLowerDash}/4.0/deed.de`
       ],
       'cclom:title': [episode.title],
-      // 'ccm:linktype': ['USER_GENERATED'],
-      // 'ccm:lifecyclecontributer_authorVCARD_COUNTRY': [''],
-      // 'ccm:lifecyclecontributer_author': ['BEGIN:VCARD\nN:Nachname;Vorname\nFN:Vorname Nachname\nVERSION:3.0\nEND:VCARD'],
-      // 'ccm:lifecyclecontributer_authorVCARD_REGION': [''],
-      // 'sys:store-protocol': ['workspace'],
-      // 'ccm:lifecyclecontributer_authorVCARD_STREET': [''],
-      // 'ccm:lifecyclecontributer_authorVCARD_PLZ': [''],
-      // 'sys:store-identifier': ['SpacesStore'],
-      // >> 'ccm:version_comment': ['LICENSE_UPDATE'],
-      // 'ccm:metadatacontributer_creatorVCARD_URL': [''],
-      // >> 'ccm:educationallearningresourcetype': ['exercise'],
       'ccm:create_version': ['true'],
       'cm:modifiedISO8601': [episode.modified],
-      // 'ccm:author_freetext': [''],
-      // 'ccm:metadatacontributer_creatorVCARD_REGION': [''],
-      // 'sys:node-dbid': ['836'],
       'ccm:wwwurl': [episode.url],
-      // 'cm:edu_metadataset': ['mds'],
-      // 'ccm:metadatacontributer_creatorVCARD_PLZ': [''],
       'cm:creator': [episode.creator],
       'cm:autoVersion': ['false'],
-      // 'virtual:permalink': ['http://localhost/edu-sharing/components/render/3ef96b84-84cb-46c9-ae0c-a6b10e27b5f2/1.3'],
-      // 'ccm:metadatacontributer_creatorVCARD_GIVENNAME': ['open'],
-      // 'cm:versionLabel': ['1.3'],
-      // 'cm:versionable': ['true'],
-      // >> 'ccm:lifecyclecontributer_authorVCARD_GIVENNAME': ['Vorname'],
-      // 'ccm:metadatacontributer_creatorVCARD_COUNTRY': [''],
-      // >> 'ccm:commonlicense_cc_version': ['4.0'],
-      // 'cm:created_LONG': ['1605880817805'],
-      // 'ccm:lifecyclecontributer_authorVCARD_TEL': [''],
       'virtual:primaryparent_nodeid': [episode.parentId],
-      // 'ccm:metadatacontributer_creator': ['BEGIN:VCARD\nVERSION:3.0\nN:cast;open\nFN:open cast\nORG:\nURL:\nTITLE:\nTEL;TYPE=WORK,VOICE:\nADR;TYPE=intl,postal,parcel,work:;;;;;;\nEMAIL;TYPE=PREF,INTERNET:\nEND:VCARD\n'],
-      // 'ccm:metadatacontributer_creatorVCARD_TEL': [''],
-      // 'ccm:lifecyclecontributer_authorVCARD_CITY': [''],
-      // 'ccm:metadatacontributer_creatorVCARD_STREET': [''],
       'cm:createdISO8601': [episode.created],
       'ccm:ph_action': ['PERMISSION_ADD'],
       'cclom:general_description': [episode.description],
-      // 'cm:modified': ['1605881127676'],
       'cm:edu_forcemetadataset': ['false'],
-      // 'ccm:metadatacontributer_creatorVCARD_CITY': [''],
       'cm:modifier': ['opencast'],
-      // 'ccm:educationallearningresourcetype_DISPLAYNAME': ['Vorlesungsaufzeichnung'],
       'cm:autoVersionOnUpdateProps': ['false'],
       'cclom:location': ['ccrep://repo/' + episode.nodeId],
-      // 'ccm:educontextname': ['default'],
       'ccm:lifecyclecontributer_authorFN': [episode.creator],
-      // 'ccm:metadatacontributer_creatorVCARD_EMAIL': [''],
-      // 'ccm:lifecyclecontributer_authorVCARD_EMAIL': [''],
       'ccm:metadatacontributer_creatorFN': ['open cast'],
-      // 'cm:modified_LONG': ['1605881127676'],
-      // 'ccm:metadatacontributer_creatorVCARD_TITLE': [''],
       'ccm:questionsallowed': ['true'],
       'cm:automaticUpdate': ['true'],
-      // 'cm:name': ['NASA_new_High_Dynamic_Range_Camera_Records_Rocket_Test.webm'],
       'cm:initialVersion': ['false'],
+      'ccm:commonlicense_key': [licenseUpperUnderscore]
+      // 'ccm:lifecyclecontributer_authorVCARD_ORG': [''],
+      // 'ccm:lifecyclecontributer_authorVCARD_TITLE': [''],
+      // 'ccm:lifecyclecontributer_authorVCARD_SURNAME': ['Nachname'],
+      // 'ccm:lifecyclecontributer_authorVCARD_COUNTRY': [''],
+      // 'ccm:lifecyclecontributer_author': ['BEGIN:VCARD\nN:Nachname;Vorname\nFN:Vorname Nachname\nVERSION:3.0\nEND:VCARD'],
+      // 'ccm:lifecyclecontributer_authorVCARD_REGION': [''],
+      // 'ccm:lifecyclecontributer_authorVCARD_STREET': [''],
+      // 'ccm:lifecyclecontributer_authorVCARD_PLZ': [''],
+      // 'ccm:lifecyclecontributer_authorVCARD_GIVENNAME': ['Vorname'],
+      // 'ccm:lifecyclecontributer_authorVCARD_TEL': [''],
+      // 'ccm:lifecyclecontributer_authorVCARD_CITY': [''],
+      // 'ccm:lifecyclecontributer_authorVCARD_EMAIL': [''],
+      // 'ccm:metadatacontributer_creatorVCARD_URL': [''],
+      // 'ccm:metadatacontributer_creatorVCARD_REGION': [''],
+      // 'ccm:metadatacontributer_creatorVCARD_PLZ': [''],
+      // 'ccm:metadatacontributer_creatorVCARD_GIVENNAME': ['open'],
+      // 'ccm:metadatacontributer_creatorVCARD_COUNTRY': [''],
+      // 'ccm:metadatacontributer_creator': ['BEGIN:VCARD\nVERSION:3.0\nN:cast;open\nFN:open cast\nORG:\nURL:\nTITLE:\nTEL;TYPE=WORK,VOICE:\nADR;TYPE=intl,postal,parcel,work:;;;;;;\nEMAIL;TYPE=PREF,INTERNET:\nEND:VCARD\n'],
+      // 'ccm:metadatacontributer_creatorVCARD_TEL': [''],
+      // 'ccm:metadatacontributer_creatorVCARD_STREET': [''],
+      // 'ccm:metadatacontributer_creatorVCARD_CITY': [''],
+      // 'ccm:metadatacontributer_creatorVCARD_EMAIL': [''],
+      // 'ccm:metadatacontributer_creatorVCARD_TITLE': [''],
+      // 'cm:created': ['1605880817805'],
+      // 'cm:modified': ['1605881127676'],
+      // 'ccm:metadatacontributer_creatorVCARD_ORG': [''],
+      // 'cclom:version': ['1.3'],
+      // 'ccm:lifecyclecontributer_authorVCARD_URL': [''],
+      // 'ccm:linktype': ['USER_GENERATED'],
+      // 'ccm:version_comment': ['LICENSE_UPDATE'],
+      // 'ccm:educationallearningresourcetype': ['exercise'],
+      // 'ccm:author_freetext': [''],
+      // 'cm:edu_metadataset': ['mds'],
+      // 'cm:versionLabel': ['1.3'],
+      // 'cm:versionable': ['true'],
+      // 'ccm:commonlicense_cc_version': ['4.0'],
+      // 'cm:created_LONG': ['1605880817805'],
+      // 'ccm:educationallearningresourcetype_DISPLAYNAME': ['Vorlesungsaufzeichnung'],
+      // 'ccm:educontextname': ['default'],
+      // 'cm:modified_LONG': ['1605881127676'],
+      // 'cm:name': ['NASA_new_High_Dynamic_Range_Camera_Records_Rocket_Test.webm'],
       // 'ccm:metadatacontributer_creatorVCARD_SURNAME': ['cast'],
       // 'cclom:general_keyword': ['Katzen', 'Hunde', 'Schlangen'],
-      'ccm:commonlicense_key': [licenseUpperUnderscore]
+      // 'sys:store-protocol': ['workspace'],
+      // 'sys:store-identifier': ['SpacesStore'],
+      // 'sys:node-dbid': ['836'],
+      // 'virtual:permalink': ['http://localhost/edu-sharing/components/render/3ef96b84-84cb-46c9-ae0c-a6b10e27b5f2/1.3'],
+      // 'virtual:commentcount': ['0'],
+      // 'virtual:usagecount': ['0'],
+      // 'virtual:childobjectcount': ['0'],
     }).toString()
   }
 
