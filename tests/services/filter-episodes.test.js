9a6df96d3e6e1df908932c26fa8dccadc2e7b402
Florian Feyen <florian@feyen.net>
2021-08-02 19:16:57 +0200

    initial testing draft with jest

---
+++ tests/services/filter-episodes.test.js
@@ -0,0 +1,19 @@
+const filter = require('../../src/services/filter-episodes')
+
+test('remove not fitting objects from array', () => {
+  const allowedLicences = ['CC0', 'CC-BY', 'CC-BY-SA', 'PD']
+
+  const episodes = [
+    { dcLicense: 'CC0' },
+    { dcLicense: 'CC-BY' },
+    { dcLicense: 'CC-BY-SA' },
+    { dcLicense: 'PD' },
+    { dcLicense: 'CC-BY-ND' },
+    { dcLicense: 'CC-BY-NC' },
+    { dcLicense: 'CC-BY-SA-NC' },
+    { dcLicense: null },
+    { dcLicense: undefined }
+  ]
+
+  expect(filter.filterAllowedLicensedEpisodes(episodes, allowedLicences).length).toBe(4)
+})
