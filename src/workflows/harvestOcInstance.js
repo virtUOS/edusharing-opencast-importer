451cead3306ed7166cb4a88fa539c7f84b44d50d
Mirjan Hoffmann <mirjan.hoffmann@tib.eu>
2022-12-14 10:43:52 +0100

    format

--- src/workflows/harvestOcInstance.js
+++ src/workflows/harvestOcInstance.js
@@ -97,7 +97,11 @@ async function harvestOcInstance(ocInstanceObj, forceUpdate) {
     episodesData = await esMetadata.updateMetadata(ocInstance, episodesData)
     storeData()
 
-    episodesData = await esUpdateThumbnails.updateThumbnails(ocInstance, episodesData, ocInstanceObj)
+    episodesData = await esUpdateThumbnails.updateThumbnails(
+      ocInstance,
+      episodesData,
+      ocInstanceObj
+    )
 
     await esPermissions.updatePermissions(ocInstance, episodesData)
 
